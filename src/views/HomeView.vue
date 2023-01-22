d516561e445e319a64609f47659064b7b9299101
Alper Kapusızoğlu <alperkapusizoglu@gmail.com>
2023-01-23 00:48:02 +0300

    blog post added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,38 @@
-<script setup></script>
+<template>
+  <div class="home">
+    <BlogPost :post="heroSection" />
+    <BlogPost v-for="(post, index) in sampleBlogPost" :key="index" :post="post" />
+  </div>
+</template>
 
-<template></template>
+<script>
+import BlogPost from '../components/BlogPost.vue';
+export default {
+  components: {
+    BlogPost,
+  },
+  data() {
+    return {
+      heroSection: {
+        title: 'Welcome',
+        blogPost:
+          'Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!',
+        heroSection: true,
+        photo: 'coding',
+      },
+      sampleBlogPost: [
+        {
+          title: 'this is a title',
+          blogHTML: 'this is a blog post title!',
+          blogCoverPhoto: 'beautiful-stories',
+        },
+        {
+          title: 'this is a second title',
+          blogHTML: 'this is a blog post title!',
+          blogCoverPhoto: 'designed-for-everyone',
+        },
+      ],
+    };
+  },
+};
+</script>
